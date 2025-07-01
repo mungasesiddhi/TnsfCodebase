ae7ae437a2da4ee2847f5160686a4cc55bac77a3
mungasesiddhi <mungasesiddhi@gmail.com>
2025-07-01 08:21:09 +0530

    Day 1

---
+++ TNSF Practice/src/com/tnsf/practice/MyFirst.java
@@ -0,0 +1,11 @@
+package com.tnsf.practice;
+
+public class MyFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helooooooo");
+
+	}
+
+}
